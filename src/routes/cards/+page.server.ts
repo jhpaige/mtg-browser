b6197395226ccfeff764a79e336e0cef639b41d6
jhpaige <jhpaige@usc.edu>
2023-03-12 22:44:20 -0700

    Got server requests to api running

--- src/routes/cards/+page.server.ts
+++ src/routes/cards/+page.server.ts
@@ -1,34 +1,41 @@
-import { error } from '@sveltejs/kit';
-
+// import { error } from '@sveltejs/kit';
+import { page } from '$app/stores';
+import { card } from 'mtgsdk';
 
 // Fetches data from API when page form submitted
 export const actions = {
-  default: async ({ request, fetch }) => {
+  default: async ({ request }) => {
+
     // Saves form data to variables
-    const submissionInfo = await request.formData();
+    const searchInfo = await request.formData();
 
-    const state = submissionInfo.get('search-select');
-    const personType = submissionInfo.get('text-input');
+    console.log(searchInfo);
 
+    const searchType = searchInfo.get('search-type');
+    const textInput = searchInfo.get('text-input');
+    const changePage = searchInfo.get('change-page');
+    const pageNumber = searchInfo.get('page-number');
+    console.log('changepage', changePage == 'true');
+
+    // Variable to store the cards' api data
+    let cardsApiData;
     // Assigns API url based on form request
-    
-    // WRITE CODE HERE
-
-    // Fetches data from api and saves to cardApiData variable
-    const cardsApiDataRes = await fetch(url);
-    const cardsApiData = await cardsApiDataRes.json();
-    console.log(cardsApiData);
-
-    // Throws error if API data is not found
-    if (cardsApiData.message) {
-      throw error(404, {
-        message: cardsApiData.message
-      });
+    if (changePage == 'true') {
+      cardsApiData = await card.where({ name: `${textInput}`, page: Number(pageNumber),  pageSize: 100 });
+      console.log(cardsApiData)
+    } else if (searchType === 'name') {
+      cardsApiData = await card.where({ name: `${textInput}`, page: 1,  pageSize: 100 });
+    } else if (searchType === 'artist') {
+      cardsApiData = await card.where({ artist: `${textInput}`, page: 1,  pageSize: 100 });
     }
+    // console.log(cardsApiData);
 
     // Returns cardsApiData as cards to page
     return {
-      cards: cardsApiData
+      cardsApiData: cardsApiData,
+      searchType: searchType,
+      textInput: textInput,
+      pageNumber: pageNumber
     }
   }
 }
