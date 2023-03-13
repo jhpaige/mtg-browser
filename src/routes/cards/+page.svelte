08cee1252f7a60c4ad47ef8e7fdbffd0546844b6
jhpaige <jhpaige@usc.edu>
2023-03-12 22:44:59 -0700

    Added pagination functionality

--- src/routes/cards/+page.svelte
+++ src/routes/cards/+page.svelte
@@ -1,102 +1,132 @@
 <script lang="ts">
 
-  import { onMount } from 'svelte';
+  import { enhance } from '$app/forms';
 
+  // Allows us to use form and data properties
+  export let form;
   export let data;
-  $: ({ cardsData } = data );
 
-  onMount(() => {
-    searchInput = document.querySelector('input');
-    searchSelect = document.getElementById('search-select');
-    searchButton = document.getElementById('search-button');
-  });
-
+  // Assigns initial card data to cardsData and isLoading/selectedItem to initial values
+  let cardsData = data.cardsData.cards;
   let isLoading = false;
+  let isChangingPage = false;
   let selectedItem = null;
+  let currPage: number = 1;
+  let changePage: boolean = false;
+  let currType: string = 'name';
 
   // Adds spaces after commas in subtypes
   function formatCommas(stringVal: string) {
     return stringVal.replace(/,/g, ', ');
   }
 
-  function handleClick(item) {
+  function handleCardClick(item) {
     isLoading = true;
     selectedItem = item;
   }
-  
-  function handleLoad() {
-    isLoading = false;
-  }
-
-  let searchInput, searchSelect, searchButton;
 
   function handleSearch() {
-    const inputValue = searchInput.value.trim();
-    const selectValue = searchSelect.value;
-
-    if (inputValue && selectValue) {
-      // Perform search
-      console.log(`Search for ${inputValue} by ${selectValue}`);
-
-      // Remove event listener to prevent further clicks
-      searchButton.removeEventListener('click', handleSearch);
-    } else {
-
-    }
+    isChangingPage = true;
+    cardsData = [];
   }
 
-  $: {
-    if (searchButton) {
-      searchButton.addEventListener('click', handleSearch);
-    }
+  function handleBackPress() {
+    currPage--;
+    cardsData = [];
+    changePage = true;
+    isChangingPage = true;
+    console.log(currPage);
+  }
+
+  function handleNextPress() {
+    currPage++;
+    cardsData = [];
+    changePage = true;
+    isChangingPage = true;
+    console.log(currPage);
+  }
+
+  function handleTypeSelection() {
+    currType = document.getElementById('search-type').value;
   }
 
 </script>
 
-<!-- <p>{JSON.stringify(cardsData.cards[0])}</p>
-<br>
-<p>{JSON.stringify(cardsData.cards[1])}</p> -->
+<!-- <p>{JSON.stringify(cardsData)}</p> -->
 
 <div class="wrapper">
 
   <h1 class="text-5xl m-10 font-black font-serif text-center">MTG Browser</h1>
 
-  <form class="relative mt-0 m-10">
+  <form class="relative m-10 flex w-screen justify-center" method="post" use:enhance={() => {
+    return async ({ result, update }) => {
+      changePage = false;
+      await update();
+      cardsData = result.data.cardsApiData;
+      currType = result.data.searchType;
+      currPage = result.data.pageNumber;
+      textInput = result.data.textInput;
+      // SET TYPE, DATA, AND PAGE NUMBER
+      isChangingPage = false;
+      isLoading = false;
+      console.log(cardsData);
+    };
+  }}>
+    <button on:click={handleBackPress} disabled={currPage == 1 && changePage == false} type="submit" class="fixed left-0 text-3xl bottom-0 h-full w-16">
+      {'<'}
+    </button>
     <div class="flex items-center border-b border-gray-500 py-2 w-80">
-      <input name='text-input' class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 focus:outline-none text-base" type="text" placeholder="Search By...">
+      <input name='text-input' class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 focus:outline-none text-base" type="text" placeholder="Search By..."/>
       <div class="relative block appearance-none w-fit bg-white border border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-        <select id="search-select" name='search-select'>
+        <select on:change={handleTypeSelection} id="search-type" name='search-type' value={currType}>
           <option value="name">Name</option>
           <option value="artist">Artist</option>
         </select>
       </div>
-      <button id="search-button" class="flex-shrink-0 apprearance-none bg-white border-gray-400 hover:border-gray-500 text-sm border mx-2 text-white py-2 px-2 rounded shadow focus:outline-none focus:shadow-outline" type="button">
+      <input name="page-number" hidden value={currPage}/>
+      <input name="change-page" hidden value={changePage}/>
+      <button id="search-button" class="flex-shrink-0 apprearance-none bg-white border-gray-400 hover:border-gray-500 text-sm border mx-2 text-white py-2 px-2 rounded shadow focus:outline-none focus:shadow-outline" type="submit" on:click={handleSearch}>
         <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
       </button>
     </div>
+    <button on:click={handleNextPress} type="submit" class="fixed right-0 text-3xl bottom-0 h-full w-16">
+      {'>'}
+    </button>
   </form>
 
-  <div class="grid">
-    {#each cardsData.cards as card}
-      {#if card.imageUrl}
-        <a href={"/cards/" + card.multiverseid} on:click={() => handleClick(card)}>
-          <div class="grid-item" class:selected={selectedItem === card}>
-            <img src={card.imageUrl} alt={card.name + " Card Photo"} on:load={handleLoad}>
-            <ul>
-              {#if card.artist}<li>Name: {card.name}</li>{/if}
-              {#if card.colors}
-                <li>Colors: {formatCommas(String(card.colors))}</li>
-              {/if}
-              {#if card.artist}<li>Artist: {card.artist}</li>{/if}
-              {#if card.subtypes}
-                <li>Subtypes: {formatCommas(String(card.subtypes))}</li>
-              {/if}
-            </ul>
-          </div>
-        </a>
-      {/if}
-    {/each}
-  </div>
+  <!-- Shows no results if no cards appear in results, otherwise displays cards -->
+  {#if cardsData.length == 0}
+    {#if isChangingPage}
+      <div class="loading-spinner"></div>
+    {:else}
+      <h2>No Results</h2>
+    {/if}
+  {:else}
+    <div class="grid">
+      <!-- Rerenders when cardsdata rerenders and loops through cards -->
+      {#key cardsData}
+        {#each cardsData as card}
+          {#if card.imageUrl}
+            <a href={"/cards/" + card.multiverseid} on:click={() => handleCardClick(card)}>
+              <div class="grid-item" class:selected={selectedItem === card}>
+                <img src={card.imageUrl} alt={card.name + " Card Photo"}>
+                <ul>
+                  {#if card.artist}<li>Name: {card.name}</li>{/if}
+                  {#if card.colors}
+                    <li>Colors: {formatCommas(String(card.colors))}</li>
+                  {/if}
+                  {#if card.artist}<li>Artist: {card.artist}</li>{/if}
+                  {#if card.subtypes}
+                    <li>Subtypes: {formatCommas(String(card.subtypes))}</li>
+                  {/if}
+                </ul>
+              </div>
+            </a>
+          {/if}
+        {/each}
+      {/key}
+    </div>
+  {/if}
 
   {#if isLoading}
     <div class="loading-overlay">
@@ -104,13 +134,13 @@
     </div>
   {/if}
 
-  <div>Pagination</div>
 </div>
 
 <style>
   .wrapper {
     align-items: center;
-    justify-content: center;
+    justify-content: top;
+    min-height: 100vh;
     display: flex;
     flex-direction: column;
     background-color: #ccc;
@@ -129,8 +159,10 @@
     align-items: center;
     column-gap: 20px;
     row-gap: 20px;
-    margin: auto;
-    width: 100%;
+    margin: 60px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    width: calc(100vw - 128px);
   }
 
   .grid-item {
@@ -187,8 +219,8 @@
     border: 4px solid #f3f3f3;
     border-top: 4px solid #3498db;
     border-radius: 50%;
-    width: 20px;
-    height: 20px;
+    width: 40px;
+    height: 40px;
     animation: spin 2s linear infinite;
     margin: auto;
   }
