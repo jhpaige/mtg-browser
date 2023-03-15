e0c893c8dad30b919cfe2bf08503df946cec043e
jhpaige <jhpaige@usc.edu>
2023-03-14 18:14:52 -0700

    Got rid of client fetching for card info data

--- src/routes/cards/[id]/+page.ts
+++
@@ -1,7 +0,0 @@
-export async function load({ fetch, params }) {
-  const cardRes = await fetch(`https://api.magicthegathering.io/v1/cards/${params.id}`);
-  const cardData = await cardRes.json();
-  return {
-    cardData: cardData
-  }
-}
